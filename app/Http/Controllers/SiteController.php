4762915b55c39d3b270954511ad6fdfc5b91cad6
Crow613 <hsaghoyan@gmail.com>
2023-12-30 14:31:47 +0400

    Set up it Laravel with home page

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+   public function home()
+   {
+     return view('site.home');
+   }
+
+   public function about()
+   {
+     // code...
+   }
+
+   public function service()
+   {
+     // code...
+   }
+
+   public function menu()
+   {
+     // code...
+   }
+
+   public function contact()
+   {
+     // code...
+   }
+}
---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+// import './bootstrap';
+import 'https://code.jquery.com/jquery-3.4.1.min.js';
+import 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js';
+import '../assets/lib/wow/wow.min.js';
+import '../assets/lib/easing/easing.min.js';
+import '../assets/lib/waypoints/waypoints.min.js';
+import '../assets/lib/counterup/counterup.min.js';
+import '../assets/lib/owlcarousel/owl.carousel.min.js';
+import '../assets/lib/tempusdominus/js/moment.min.js';
